126b75522b7be6ce195a021defea26dfa4a9c655
Arthur Maltson <arthur@maltson.com>
2014-06-08 12:47:28 -0400

    Create db seed for the CV service

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create the CV web service
+Service.create!(name: 'Commuted Value Web Service',
+  group: 'Core Service',
+  jira: 'http://jira',
+  confluence: 'http://confluence/display/SYS/Commuted+Value+Web-Service')
