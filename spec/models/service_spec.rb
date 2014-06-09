74866476b2b9d561b88fb2c0c3d6e21b4f5c836a
Arthur Maltson <arthur@maltson.com>
2014-06-09 15:36:07 -0400

    Add relationship between Services and Instances

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Service, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Service, type: :model do
+  let(:service) do
+    service = Service.new(name: 'CV', group: 'Core')
+    service.instances << Instance.create!(name: 'CV 4', version_of_artifact: '4')
+    service
+  end
+
+  it "should save a new service with instances" do
+    expect(service.name).to eq('CV')
+    expect(service.group).to eq('Core')
+    expect(service.instances.size).to eq(1)
+    expect(service.instances.first.name).to eq('CV 4')
+  end
 end
