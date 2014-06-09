5c66f51ece796bfe1bef0cee33f039af39c9972c
Arthur Maltson <arthur@maltson.com>
2014-06-09 17:36:39 -0400

    Implement getting a specific service
    - add integration tests

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,4 +2,8 @@ class ServicesController < ApplicationController
   def index
     render json: Service.all, status: :ok
   end
+
+  def show
+    render json: Service.find(params[:id]), status: :ok
+  end
 end
